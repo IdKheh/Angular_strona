e30981613b08915c4c773e010d7cae4ec80db13e
IdKheh <chemik223@gmail.com>
2024-01-28 13:12:21 +0100

    Change contetnt of page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
+import $ from 'jquery';
+import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  number: number = 0;
+  amountOfPhotos: number = 7;
+  photoPath: string ='assets/img/photo1.png';
+  timer1:NodeJS.Timeout | undefined;
+  timer2:NodeJS.Timeout | undefined;
+
+  createDots():void{
+    const app = document.getElementById("dot");
+    for(let i=1;i<=1;i++){
+      let dot = document.createElement('div');
+      dot.classList.add('dots');
+      dot.style.width='25px';
+      dot.style.height='25px';
+      dot.style.border='2px solid white';
+      dot.style.float='left';
+      dot.style.margin= '0vh 1%';
+      dot.textContent=this.number.toString();
+
+      app?.appendChild(dot);
+    }
+  }
+  hidePhoto():void{
+   $('#gallery').fadeIn(500);
+  }
+
+  showPhoto():void{
+    if(this.number>this.amountOfPhotos){
+      this.number=1;
+    }
+    this.photoPath ='assets/img/photo'+this.number+'.png';
+    this.number++;
+
+    $('#gallery').fadeOut(500);
+  }
+
+
+  start():void{
+    //TODO: Fix below lines
+    this.timer1=setTimeout(() => this.showPhoto(),4000);
+    this.timer2=setTimeout(() => this.hidePhoto(),3500);
+  }
+
+  @HostListener('window:load')
+  onLoad() {
+    this.start();
+  }
+
+
 
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
+    password:string= 'costam';
+    passDisp:string ='';
+    photoPath:string='assets/hangman/photo5.png';
+    message:string= '';
+    nameOfButton:string='Start';
+    letterToWin:number=0;
+    displayedPassword:Array<string>=[];
+    lives:number=5;
+
+    setup():void{
+        const n:number = this.password.length;
+        this.letterToWin = this.password.length;
+        for(let i=0;i<n;i++){
+            this.displayedPassword[i]='_';
+        }
+        this.passDisp= this.displayedPassword.join(' ');
+        this.lives=5;
+        this.photoPath = "assets/hangman/photo"+this.lives+".png";
+        this.nameOfButton='Restart';
+    }
+    game():void{
+        const n:number = this.password.length;
+        if(this.nameOfButton!='Start'){
+            this.message='';
+            let letter = (document.getElementById('letter') as HTMLInputElement).value;
+            let done:number=0;
+            if(letter.length==0){
+                this.message='Enter the letter!';
+            }
+            else{
+                for(let i=0;i<n;i++){
+                    if(this.password[i]==letter && this.displayedPassword[i]!=letter){
+                        this.displayedPassword[i]=letter;
+                        this.passDisp= this.displayedPassword.join(' ');
+                        this.letterToWin--;
+                        done=1;
+                        this.message='';
+                    }
+                    else if(this.password[i]==letter && this.displayedPassword[i]==letter){
+                        this.message='You have already guessed this letter!';
+                        done=1;
+                    }
+                }
+            }
+            (document.getElementById('letter') as HTMLInputElement).value='';
+            if(!done){
+                this.lives--;
+                this.photoPath = "assets/hangman/photo"+this.lives+".png";
+            }
+
+            if(this.letterToWin==0 && this.lives>0){
+                this.message="Winner!! Congratulations!!";
+            }
+            else if(this.lives==0){
+                this.message="You are loose!";
+            }
+        }
+    }
+
 
 }
